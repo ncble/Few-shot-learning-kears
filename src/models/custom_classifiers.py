8e729ad19b3b60a783cbe62ca4eea0b5dec2442c
ZHANGyuan <fern35121321@outlook.com>
2019-01-04 00:36:31 +0100

    add custom layers, cosine based classifier

---
+++ src/models/custom_classifiers.py
@@ -0,0 +1,9 @@
+from keras import backend
+
+
+def cos_sim_classifier(feature_extraxted,n_classes):
+    """
+    :param feature_extraxted: the input tensor
+    :param n_classes:
+    :return:
+    """
--- test.py
+++ test.py
@@ -1,14 +1,41 @@
-from src.models.backbone import ResNet10
+from src.models.backbones import ResNet10
+from keras import backend as K
+import tensorflow as tf
+##################################
+# test backbone
+##################################
+# model1 = ResNet10(include_top=True,
+#              input_shape=(224,224,3),
+#              pooling=None,
+#              classes=10)
+# print(model1.summary())
+#
+#
+# model2 = ResNet10(include_top=False,
+#              input_shape=(224,224,3),
+#              pooling=True,
+#              classes=10)
+# print(model2.summary())
 
-model1 = ResNet10(include_top=True,
-             input_shape=(224,224,3),
-             pooling=None,
-             classes=10)
-print(model1.summary())
 
+#######################################
+# check cos_dense layer
+def l2_norm(x, axis=None):
+    """
+    takes an input tensor and returns the l2 norm along specified axis
+    """
+    square_sum = K.sum(K.square(x), axis=axis, keepdims=True)
+    norm = K.sqrt(K.maximum(square_sum, K.epsilon()))
 
-model2 = ResNet10(include_top=False,
-             input_shape=(224,224,3),
-             pooling=True,
-             classes=10)
-print(model2.summary())
+    return norm
+
+x = K.placeholder(shape=(15, 4))
+y = K.placeholder(shape=(4, 2))
+xy = K.dot(x,y)
+x_norm = l2_norm(x,axis=-1)
+y_norm = l2_norm(y,axis=0)
+xy_norm = K.dot(x_norm,y_norm)
+
+cos_similarity = xy/xy_norm
+print(cos_similarity)
+#######################################
