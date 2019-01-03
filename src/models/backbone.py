4b3bb3210c37527d1f72cfdc21795ca124ef7aa1
ZHANGyuan <fern35121321@outlook.com>
2019-01-03 17:28:41 +0100

    update backbone, add resnet10

---
+++ src/models/backbone.py
@@ -0,0 +1,111 @@
+from keras import backend
+from keras import layers
+from keras import models
+
+
+def conv_block2(input_tensor,
+               kernel_size,
+               filters,
+               stage,
+               block,
+               strides=(2, 2)):
+    """A block that has a conv layer at shortcut.
+
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of
+            middle conv layer at main path
+        filters: list of integers, the filters of 2 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+        strides: Strides for the first conv layer in the block.
+
+    # Returns
+        Output tensor for the block.
+
+    Note that from stage 3,
+    the first conv layer at main path is with strides=(2, 2)
+    And the shortcut should have strides=(2, 2) as well
+    """
+    filters1, filters2 = filters
+    if backend.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = layers.Conv2D(filters1, kernel_size, strides=strides,
+                      kernel_initializer='he_normal',
+                      name=conv_name_base + '2a')(input_tensor)
+    x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv2D(filters2, kernel_size, padding='same',
+                      kernel_initializer='he_normal',
+                      name=conv_name_base + '2b')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    # x = layers.Activation('relu')(x)
+    #
+    # x = layers.Conv2D(filters3, (1, 1),
+    #                   kernel_initializer='he_normal',
+    #                   name=conv_name_base + '2c')(x)
+    # x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = layers.Conv2D(filters2, (1, 1), strides=strides,
+                             kernel_initializer='he_normal',
+                             name=conv_name_base + '1')(input_tensor)
+    shortcut = layers.BatchNormalization(
+        axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = layers.add([x, shortcut])
+    x = layers.Activation('relu')(x)
+    return x
+
+
+def ResNet10(include_top=True,
+             input_shape=None,
+             pooling=None,
+             classes=10):
+    # channel last
+    img_input = layers.Input(shape=input_shape)
+    if backend.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    x = layers.ZeroPadding2D(padding=(3, 3), name='conv1_pad')(img_input)
+    x = layers.Conv2D(64, (7, 7),
+                      strides=(2, 2),
+                      padding='valid',
+                      kernel_initializer='he_normal',
+                      name='conv1')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = layers.Activation('relu')(x)
+    x = layers.ZeroPadding2D(padding=(1, 1), name='pool1_pad')(x)
+    x = layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = conv_block2(x, 3, [64, 64], stage=2, block='a', strides=(1, 1))
+
+    x = conv_block2(x, 3, [128, 128], stage=3, block='a')
+
+    x = conv_block2(x, 3, [256, 256], stage=4, block='a')
+
+    x = conv_block2(x, 3, [512, 512], stage=5, block='a')
+
+    if include_top:
+        x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+        x = layers.Dense(classes, activation='softmax', name='fc1000')(x)
+    else:
+        if pooling == 'avg':
+            x = layers.GlobalAveragePooling2D()(x)
+        elif pooling == 'max':
+            x = layers.GlobalMaxPooling2D()(x)
+        else:
+            print('The output shape of `ResNet10(include_top=False)` '
+                          'has been changed ')
+
+    inputs = img_input
+    model = models.Model(inputs, x, name='resnet10')
+
+    return model
