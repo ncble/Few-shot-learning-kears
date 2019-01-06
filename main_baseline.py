964b2cfed590e1b80724571e62369e43cdc4b986
ZHANGyuan <fern35121321@outlook.com>
2019-01-06 23:37:06 +0100

    update code for experiment

--- main_baseline.py
+++ main_baseline.py
@@ -9,59 +9,59 @@ import keras.backend as K
 
 #
 # ####################### training stage ###################
-# # load image
-# train_train_path = 'miniImageNet_category_split_train_phase_train.pickle'
-# train_val_path = 'miniImageNet_category_split_train_phase_val.pickle'
-#
-# train_train = loader.load_miniImgnet(train_train_path)
-# train_val = loader.load_miniImgnet(train_val_path)
-#
-# train_img = train_train[b'data']
-# train_label = train_train[b'labels']
-#
-# val_img = train_val[b'data']
-# val_label = train_val[b'labels']
-#
-# # define input shape, classes
-# input_shape = train_img.shape[1:]
-# #print(input_shape)
-# classes = len(train_train[b'catname2label'])
-# #print(classes)
-#
-#
-# # baseline
-# baseline = Baseline(input_shape=input_shape,classes=classes)
-# baseline.compile(loss='sparse_categorical_crossentropy', optimizer=optimizers.Adam(lr=1e-3),
-#               metrics=['accuracy'])
-#
-# history_baseline = trainer.train_model(model=baseline,
-#                 x=train_img,y=train_label,
-#                 shuffle=True,
-#                 val_data=(val_img,val_label),
-#                 batch_size=64,
-#                 epochs=400 ,
-#                 weights_file='baseline_training.h5', # save weights
-#                 monitor='val_acc',
-#                 )
-#
-# pickle.dump(history_baseline,open('output/history_baseline.pickle','wb'))
-#
-# # baseline_plus
-# baseline_plus = Baseline_plus(input_shape=input_shape,classes=classes)
-# baseline_plus.compile(loss='sparse_categorical_crossentropy',
-#                       optimizer=optimizers.Adam(lr=1e-3),
-#                       metrics=['accuracy'])
-# history_baseline_plus = trainer.train_model(model=baseline_plus,
-#                 x=train_img,y=train_label,
-#                 shuffle=True,
-#                 val_data=(val_img,val_label),
-#                 batch_size=64,
-#                 epochs=400 ,
-#                 weights_file='baseline_plus_training.h5',
-#                 monitor='val_acc',
-#                 )
-# pickle.dump(history_baseline_plus,open('output/history_baseline_plus.pickle','wb'))
-#
+# load image
+train_train_path = 'miniImageNet_category_split_train_phase_train.pickle'
+train_val_path = 'miniImageNet_category_split_train_phase_val.pickle'
+
+train_train = loader.load_miniImgnet(train_train_path)
+train_val = loader.load_miniImgnet(train_val_path)
+
+train_img = train_train[b'data']
+train_label = train_train[b'labels']
+
+val_img = train_val[b'data']
+val_label = train_val[b'labels']
+
+# define input shape, classes
+input_shape = train_img.shape[1:]
+#print(input_shape)
+classes = len(train_train[b'catname2label'])
+#print(classes)
+
+
+# baseline
+baseline = Baseline(input_shape=input_shape,classes=classes)
+baseline.compile(loss='sparse_categorical_crossentropy', optimizer=optimizers.Adam(lr=1e-3),
+              metrics=['accuracy'])
+
+history_baseline = trainer.train_model(model=baseline,
+                x=train_img,y=train_label,
+                shuffle=True,
+                val_data=(val_img,val_label),
+                batch_size=64,
+                epochs=400 ,
+                weights_file='baseline_training.h5', # save weights
+                monitor='val_acc',
+                )
+
+pickle.dump(history_baseline,open('output/history_baseline.pickle','wb'))
+
+# baseline_plus
+baseline_plus = Baseline_plus(input_shape=input_shape,classes=classes)
+baseline_plus.compile(loss='sparse_categorical_crossentropy',
+                      optimizer=optimizers.Adam(lr=1e-3),
+                      metrics=['accuracy'])
+history_baseline_plus = trainer.train_model(model=baseline_plus,
+                x=train_img,y=train_label,
+                shuffle=True,
+                val_data=(val_img,val_label),
+                batch_size=64,
+                epochs=400 ,
+                weights_file='baseline_plus_training.h5',
+                monitor='val_acc',
+                )
+pickle.dump(history_baseline_plus,open('output/history_baseline_plus.pickle','wb'))
+
 
 ##################### fine-tuning stage ######################
 
