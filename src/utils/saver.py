3dfcf490d548be158bcd7516313b550e40786123
ZHANGyuan <fern35121321@outlook.com>
2019-01-07 19:18:09 +0100

    update code for experiment

--- src/utils/saver.py
+++ src/utils/saver.py
@@ -5,4 +5,4 @@ base_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..','..')
 output_folder_path = os.path.join(base_path,'output')
 
 def save_output(obj,filename):
-    return pickle.dump(obj,open(os.path.join(output_folder_path,filename),'rb'))
+    return pickle.dump(obj,open(os.path.join(output_folder_path,filename),'wb'))
