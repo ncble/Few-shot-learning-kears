f1520ff1cdc0c62ebee1fb65ad7cd10b87673296
ZHANGyuan <fern35121321@outlook.com>
2019-01-07 20:51:28 +0100

    update code for experiment

--- main_baseline.py
+++ main_baseline.py
@@ -27,7 +27,7 @@ val_label = train_val[b'labels']
 # define input shape, classes
 input_shape = train_img.shape[1:]
 classes = len(train_train[b'catname2label'])
-train_epoch = 400
+train_epoch = 1
 
 # baseline
 baseline = Baseline(input_shape=input_shape,classes=classes)
